068817eae3b7abfbb66d77910496e4cbb65b2a72
edire <eric.di.re@gmail.com>
2023-10-31 13:33:16 -0700

    moved manifest.in file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dbharbor',
     packages=['dbharbor'],
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     author='Eric Di Re',
     author_email='eric.dire@direanalytics.com',
