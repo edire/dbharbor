f456506c2360dcef9f5f1be23b120f1711516f1b
edire <eric.di.re@gmail.com>
2024-09-13 14:02:52 -0700

    updated version and readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dbharbor',
     packages=['dbharbor'],
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
     author='Eric Di Re',
     author_email='eric.dire@direanalytics.com',
